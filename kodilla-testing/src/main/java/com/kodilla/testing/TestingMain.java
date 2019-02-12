f1d1fe1373b5433f4e1403f230ebcc7bef430eb7
Marek Dąbkowski <marquess94@gmail.com>
2019-02-12 18:57:27 +0100

    Thirs Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,16 @@
 package com.kodilla.testing;
 
 import com.kodilla.testing.User.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
         SimpleUser simpleUser = new SimpleUser("theForumUser");
+        Calculator calculator= new Calculator();
+
+
+    int math = calculator.add(4,5);
+    int math2 = calculator.substract(6,7);
 
 
     String result = simpleUser.getUsername();
@@ -15,4 +21,8 @@ public class TestingMain {
         System.out.println("Error");
         }
     }
+
+
+
+
 }
